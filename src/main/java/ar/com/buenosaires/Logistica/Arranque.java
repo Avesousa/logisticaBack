619e4a0ba6c2909ae2c37fd47eedeb7250736745
Avelino Figueira <37675539+Avesousa@users.noreply.github.com>
2020-02-11 12:21:51 -0300

    Add files via upload

---
+++ src/main/java/ar/com/buenosaires/Logistica/Arranque.java
@@ -0,0 +1,13 @@
+package ar.com.buenosaires.Logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arranque {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arranque.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/buenosaires/Logistica/SedLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.buenosaires.Logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
